fa8a450a38e0b7fddf522d25773928e8f116d247
hsandstromOM <hosea@obviouslee.com>
2017-07-31 13:01:25 -0400

    minimize

--- config/sharedPlugins.js
+++ config/sharedPlugins.js
@@ -1,7 +1,23 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
 const plugins = [
+  new webpack.LoaderOptionsPlugin({
+    minimize: true,
+    debug: false
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+   beautify: false,
+   mangle: {
+     screw_ie8: true,
+     keep_fnames: true
+   },
+   compress: {
+     screw_ie8: true
+   },
+   comments: false
+ }),
   new HtmlWebpackPlugin({
     template: path.join(__dirname, '/../app/index.html'),
     inject: 'body',
